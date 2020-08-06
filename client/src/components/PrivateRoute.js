cdd0bdd73aadee90ebce9bf1a9c219200e6c820f
Carlos 'Mannie' Alvarez <carlos.alvarezberrio@gmail.com>
2020-08-05 20:30:50 -0400

    privateroute

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivateRoute = ({ Component: component, ...props }) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivateRoute;
